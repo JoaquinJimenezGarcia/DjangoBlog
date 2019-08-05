d1a44f97e539fde2e0ab36e3a42ed93ca558f714
Joaquin Jimenez Garcia <garcia.jjimenez@gmail.com>
2019-08-05 18:46:55 +0200

    Added posts to templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import BlogPost
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = BlogPost.objects.filter(timestamp__lte=timezone.now()).order_by('timestamp')
+    print(posts)
+    return render(request, 'blog/post_list.html', {'posts': posts})
